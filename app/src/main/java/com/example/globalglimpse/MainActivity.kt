453150603ac437f1629d26ea156aef84229a03e8
Zeyad_Absattar <zeyadabdelsattar@gmail.com>
2024-02-08 03:12:14 +0200

    #3 Set the dark theme for the app

--- app/src/main/java/com/example/globalglimpse/MainActivity.kt
+++ app/src/main/java/com/example/globalglimpse/MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.globalglimpse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import com.example.globalglimpse.ui.theme.GlobalGlimpseTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GlobalGlimpseTheme {
-                var showWelcomeScreen by remember { mutableStateOf(true) }
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    var showWelcomeScreen by remember { mutableStateOf(true) }
 
-                if (showWelcomeScreen) {
-                    WelcomeScreen(onProceed = { showWelcomeScreen = false })
-                } else {
-                   InformationScreen()
+                    if (showWelcomeScreen) {
+                        WelcomeScreen(onProceed = { showWelcomeScreen = false })
+                    } else {
+                        InformationScreen()
+                    }
                 }
             }
         }
